9f8740e3354578ff9419dc8856ebe61dede6440d
vennakag <vennakag@student.touro.edu>
2025-11-12 22:10:05 -0500

    Project-infixToPostfix

--- ds-starter/S4-Infix-To-Postfix/src/main/java/edu/touro/mcon264/s4_infix_to_postfix/Solution.java
+++ ds-starter/S4-Infix-To-Postfix/src/main/java/edu/touro/mcon264/s4_infix_to_postfix/Solution.java
@@ -1,5 +1,89 @@
 package edu.touro.mcon264.s4_infix_to_postfix;
 
+import java.util.Stack;
+
 public class Solution {
     // TODO: Implement solution for: Infix → Postfix + Evaluate
+    private StringBuilder token = new StringBuilder();
+    Stack<Character> operatorStack = new Stack<>();
+    private String expression;
+
+    public Solution(String expression){
+        this.expression = expression;
+    }
+
+    
+    public int operand(char expChar){
+        switch (expChar) {
+            case '+':
+            case '-':
+                return 1;
+            case '*':
+            case '/':
+                return 2;
+            default:
+                return -1;
+        }
+    }
+
+    public String infix_to_postfix(){
+        if(expression==null){
+            throw new IllegalStateException("Expression is missing");
+        }else{
+            char[] expressionCharacters = expression.toCharArray();
+            for(int i=0; i<expressionCharacters.length;i++) {
+                if (Character.isWhitespace(expressionCharacters[i])) continue;
+                if(Character.isDigit(expressionCharacters[i])){
+                    token.append(expressionCharacters[i]);
+                }else if (expressionCharacters[i] == '+' || expressionCharacters[i] == '-'
+                        || expressionCharacters[i] == '/' || expressionCharacters[i] == '*') {
+                    if(!operatorStack.isEmpty()&&operand(operatorStack.peek()) >= operand(expressionCharacters[i])){
+                        token.append(operatorStack.pop());
+                    }
+                    operatorStack.push(expressionCharacters[i]);
+                    }
+            }
+            while (!operatorStack.isEmpty()) {
+                token.append(operatorStack.pop());
+            }
+        }
+        return token.toString();
+    }
+
+    public int evaluate_postfix(String expression){
+        char[] tokens = expression.toCharArray();
+        Stack<Integer> s = new Stack<>();
+        if(tokens.length==0){
+            throw new IllegalStateException("Expression is missing");
+        }else{
+            for(int i = 0; i < tokens.length;i++){
+                if (Character.isDigit(tokens[i])){
+                    s.push(tokens[i] - '0');
+                }else{
+                    if(tokens[i]=='+'){
+                      s.push(s.pop()+s.pop()) ;
+                    }else if(tokens[i]=='-'){
+                        int b = s.pop();
+                        int a = s.pop();
+                        s.push(a-b) ;
+                    }else if(tokens[i]=='/'){
+                        int b = s.pop();
+                        int a = s.pop();
+                        s.push(a/b) ;
+                    }else if(tokens[i]=='*'){
+                        s.push(s.pop()*s.pop()) ;
+                    }
+                }
+            }
+        }
+        return s.pop();
+    }
+
+
+    public static void main(String[] args) {
+        Solution s = new Solution("3+9+5");
+        System.out.println(s.infix_to_postfix());
+        Solution s2 = new Solution("4+12");
+        System.out.println(s2.infix_to_postfix());
+    }
 }
--- ds-starter/S4-Infix-To-Postfix/src/test/java/edu/touro/mcon264/s4_infix_to_postfix/SolutionTest.java
+++ ds-starter/S4-Infix-To-Postfix/src/test/java/edu/touro/mcon264/s4_infix_to_postfix/SolutionTest.java
@@ -1,10 +1,15 @@
 package edu.touro.mcon264.s4_infix_to_postfix;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.Test;
 
 class SolutionTest {
     @Test
-    void testPlaceholder() {
+    void testInfix_to_postfix() {
         // TODO: Add tests for Infix → Postfix + Evaluate
-    }
+        Solution s = new Solution("3+9+5");
+        assertEquals("39+5+", s.infix_to_postfix());
+        assertEquals(17, s.evaluate_postfix(s.infix_to_postfix()));
+        }
+
 }

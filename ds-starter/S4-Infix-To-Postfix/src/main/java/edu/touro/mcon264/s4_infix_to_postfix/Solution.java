97f58f7732d51c7c1cb151717423e481e8024408
vennakag <vennakag@student.touro.edu>
2026-01-11 13:57:56 -0500

    Fixed Mistakes

--- ds-starter/S4-Infix-To-Postfix/src/main/java/edu/touro/mcon264/s4_infix_to_postfix/Solution.java
+++ ds-starter/S4-Infix-To-Postfix/src/main/java/edu/touro/mcon264/s4_infix_to_postfix/Solution.java
@@ -34,56 +34,67 @@ public class Solution {
             for(int i=0; i<expressionCharacters.length;i++) {
                 if (Character.isWhitespace(expressionCharacters[i])) continue;
                 if(Character.isDigit(expressionCharacters[i])){
-                    token.append(expressionCharacters[i]);
+                    while (i < expressionCharacters.length && Character.isDigit(expressionCharacters[i])) {
+                        token.append(expressionCharacters[i]);
+                        i++;
+                    }
+                    token.append(' ');
+                    i--;
                 }else if (expressionCharacters[i] == '+' || expressionCharacters[i] == '-'
                         || expressionCharacters[i] == '/' || expressionCharacters[i] == '*') {
                     if(!operatorStack.isEmpty()&& operandPrecedence(operatorStack.peek()) >= operandPrecedence(expressionCharacters[i])){
-                        token.append(operatorStack.pop());
+                        token.append(operatorStack.pop()).append(' ');
                     }
                     operatorStack.push(expressionCharacters[i]);
                     }
             }
             while (!operatorStack.isEmpty()) {
-                token.append(operatorStack.pop());
+                token.append(operatorStack.pop()).append(' ');
             }
         }
-        return token.toString();
+        return token.toString().trim();
     }
 
     public int evaluate_postfix(String expression){
-        char[] tokens = expression.toCharArray();
+        String[] tokens = expression.trim().split("\\s+");
         Stack<Integer> s = new Stack<>();
         if(tokens.length==0){
             throw new IllegalStateException("Expression is missing");
         }else{
             for(int i = 0; i < tokens.length;i++){
-                if (Character.isDigit(tokens[i])){
-                    s.push(tokens[i] - '0');
+                String token = tokens[i];
+                if (token.matches("\\d+")){
+                    s.push(Integer.parseInt(token));
                 }else{
-                    if(tokens[i]=='+'){
+                    if(token.equals("+")){
                         if (s.size() < 2) {
                             throw new IllegalArgumentException("Malformed postfix expression: not enough operands for '+'");
                         }
                         s.push(s.pop() + s.pop());
-                    }else if(tokens[i]=='-'){
+                    }else if(token.equals("-")){
                         if (s.size() < 2) {
                             throw new IllegalArgumentException("Malformed postfix expression: not enough operands for '-'");
                         }
                         int b = s.pop();
                         int a = s.pop();
                         s.push(a-b) ;
-                    }else if(tokens[i]=='/'){
+                    }else if(token.equals("*")){
                         if (s.size() < 2) {
                             throw new IllegalArgumentException("Malformed postfix expression: not enough operands for '/'");
                         }
                         int b = s.pop();
                         int a = s.pop();
-                        s.push(a/b) ;
-                    }else if(tokens[i]=='*'){
+                        s.push(a*b) ;
+                    }else if(token.equals("/")){
                         if (s.size() < 2) {
                             throw new IllegalArgumentException("Malformed postfix expression: not enough operands for '*'");
                         }
-                        s.push(s.pop()*s.pop()) ;
+                        int b = s.pop();
+                        int a = s.pop();
+                        if (b == 0) {
+                            throw new IllegalArgumentException("Division by zero is not allowed.");
+                        }
+                        s.push(a/b) ;
                     }
                 }
             }
